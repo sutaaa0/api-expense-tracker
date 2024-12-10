e4a2fc8e5515bf7f844ecc1cc24201683d821bb8
sutaaa0 <sutadev0@gmail.com>
2024-12-10 16:51:56 +0700

    add monthly transaction service

--- src/transaction/transaction.service.js
+++ src/transaction/transaction.service.js
@@ -1,4 +1,4 @@
-const { findTransactionsByUser, insertTransaction } = require("./transaction.repository");
+const { findTransactionsByUser, insertTransaction, findMonthlyTransactionsByUser } = require("./transaction.repository");
 
 const getTransactionsByUser = async (userId) => {
     const transactions = await findTransactionsByUser(userId);
@@ -9,6 +9,20 @@ const getTransactionsByUser = async (userId) => {
 
     return transactions
 }
+
+const getMonthlyTransactionsByUser = async (userId) => {
+    const transactions = await findMonthlyTransactionsByUser(userId);
+  
+    if (!transactions || transactions.length === 0) {
+      throw new Error("No transactions found for this month");
+    }
+  
+    return transactions.map((transaction) => ({
+      category: transaction.category,
+      totalAmount: transaction._sum.amount,
+    }));
+  };
+  
 
 const createTransaction = async (transaction) => {
     const newTransaction = await insertTransaction(transaction);
@@ -22,4 +36,4 @@ const updateTransaction = async (id, transaction) => {
     return updatedTransaction
 }
 
-module.exports = { getTransactionsByUser, createTransaction };
+module.exports = { getTransactionsByUser, createTransaction, getMonthlyTransactionsByUser };
