d514d29d4a804d6712629e2ca461341e0a5808e4
sutaaa0 <sutadev0@gmail.com>
2024-12-10 15:56:03 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
+
 
 dotenv.config();
 
+
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 app.get("/api", (req, res) => {
