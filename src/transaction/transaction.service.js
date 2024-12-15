9301bfb4399c1f1421ef77b37748024122c2218c
sutaaa0 <sutadev0@gmail.com>
2024-12-15 10:15:54 +0700

    add getIncomeMonthly service

--- src/transaction/transaction.service.js
+++ src/transaction/transaction.service.js
@@ -1,49 +1,61 @@
-const { findTransactionsByUser, insertTransaction, findMonthlyTransactionsByUser, findIncomeTransactionsByUser } = require("./transaction.repository");
+const { findTransactionsByUser, insertTransaction, findMonthlyTransactionsByUser, findIncomeTransactionsByUser, findMonthlyIncomeTransactionsByUser } = require("./transaction.repository");
 
 const getTransactionsByUser = async (userId) => {
-    const transactions = await findTransactionsByUser(userId);
+  const transactions = await findTransactionsByUser(userId);
 
-    if (!transactions) {
-        throw new Error("User not found");
-    }
+  if (!transactions) {
+    throw new Error("User not found");
+  }
 
-    return transactions
-}
+  return transactions;
+};
 
 const getIncomeTransactionsByUser = async (userId) => {
-    const transactions = await findIncomeTransactionsByUser(userId);
-  
-    if (!transactions || transactions.length === 0) {
-      throw new Error("No transactions found for this month");
-    }
-  
-    return transactions.filter((transaction) => transaction.type === "income");
-  };
+  const transactions = await findIncomeTransactionsByUser(userId);
+
+  if (!transactions || transactions.length === 0) {
+    throw new Error("No transactions found for this month");
+  }
+
+  return transactions.filter((transaction) => transaction.type === "income");
+};
+
+const getMonthlyIncomeByUser = async (userId) => {
+  const incomes = await findMonthlyIncomeTransactionsByUser(userId);
+
+  if (!incomes || transactions.incomes === 0) {
+    throw new Error("No income found for this month");
+  }
+
+  return incomes.map((income) => ({
+    category: income.category,
+    totalAmount: income._sum.amount,
+  }));
+};
 
 const getMonthlyTransactionsByUser = async (userId) => {
-    const transactions = await findMonthlyTransactionsByUser(userId);
-  
-    if (!transactions || transactions.length === 0) {
-      throw new Error("No transactions found for this month");
-    }
-  
-    return transactions.map((transaction) => ({
-      category: transaction.category,
-      totalAmount: transaction._sum.amount,
-    }));
-  };
-  
+  const transactions = await findMonthlyTransactionsByUser(userId);
+
+  if (!transactions || transactions.length === 0) {
+    throw new Error("No transactions found for this month");
+  }
+
+  return transactions.map((transaction) => ({
+    category: transaction.category,
+    totalAmount: transaction._sum.amount,
+  }));
+};
 
 const createTransaction = async (transaction) => {
-    const newTransaction = await insertTransaction(transaction);
+  const newTransaction = await insertTransaction(transaction);
 
-    return newTransaction
-}
+  return newTransaction;
+};
 
 const updateTransaction = async (id, transaction) => {
-    const updatedTransaction = await editTransaction(id, transaction);
+  const updatedTransaction = await editTransaction(id, transaction);
 
-    return updatedTransaction
-}
+  return updatedTransaction;
+};
 
-module.exports = { getTransactionsByUser, createTransaction, getMonthlyTransactionsByUser, getIncomeTransactionsByUser };
+module.exports = { getTransactionsByUser, createTransaction, getMonthlyTransactionsByUser, getIncomeTransactionsByUser, getMonthlyIncomeByUser };
